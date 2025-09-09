fdf643ca13c6f65f55e5a0bfaf5d5fa9b9c6568d
Andy de Oliveira <elderromero565@gmail.com>
2025-09-08 21:37:54 -0300

    xxxxxxxxxxxxSDSSxxxxxxxxxxxxxxx

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  base: '/',   // 🔹 sempre raiz
   plugins: [react()],
 })
